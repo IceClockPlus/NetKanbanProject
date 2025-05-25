167eb420b050b1ea11c3cf226e9b2dc240be77bb
Alexis Aravena <aravenapro98@gmail.com>
2025-05-25 18:24:11 -0400

    Create global error middleware to handle errors

---
+++ src/API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+namespace API.Middlewares
+{
+    using System.Net;
+    using System.Text.Json;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Middleware for handling exceptions globally.
+    /// </summary>
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            _logger.LogError(exception, "An unhandled exception occurred.");
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An unexpected error occurred. Please try again later.",
+                Detail = exception.Message,
+            };
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
