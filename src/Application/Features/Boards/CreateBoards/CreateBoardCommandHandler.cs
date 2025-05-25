0e1f293b302fdfb0bbf31c50b4870e257830a584
Alexis Aravena <aravenapro98@gmail.com>
2025-05-24 20:28:28 -0400

    Initial implementation new command to create new Boards

--- src/Application/Features/Boards/CreateBoards/CreateBoardCommandHandler.cs
+++ src/Application/Features/Boards/CreateBoards/CreateBoardCommandHandler.cs
@@ -1,3 +1,6 @@
+using Application.Interfaces;
+using Domain.Boards;
+
 namespace Application.Features.Boards.CreateBoards
 {
 
@@ -6,6 +9,35 @@ namespace Application.Features.Boards.CreateBoards
     /// </summary>
     public class CreateBoardCommandHandler
     {
-        
+        private readonly IBoardRepository _boardRepository;
+        public CreateBoardCommandHandler(IBoardRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+
+        public async Task<Guid> Handle(CreateBoardCommand command, CancellationToken cancellationToken)
+        { 
+            try
+            {
+                var guid = Guid.NewGuid();
+                var boardToRegister = new Board(
+                    id: guid,
+                    name: command.Name,
+                    description: command.Description,
+                    createdAt: DateTime.UtcNow,
+                    updatedAt: null
+                );
+
+                await _boardRepository.CreateBoardAync(boardToRegister, cancellationToken);
+                return boardToRegister.Id;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating board: {ex.Message}");
+                // Optionally log the exception here
+                throw;
+            }
+        }
+
     }
 }
