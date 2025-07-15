b88e6c2460dd42df674d05ab9b5317152b496205
Alexis Aravena <aravenapro98@gmail.com>
2025-07-14 20:59:27 -0400

    Change to request to Update user account info

---
+++ src/Contracts/Users/Request/UpdateUserAccountRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Users.Request
+{
+    public record UpdateUserAccountRequest
+    {
+        public string FirstName { get; init; } = string.Empty;
+        public string? SecondName { get; init; }
+        public string LastName { get; init; } = string.Empty;
+
+    }
+}
